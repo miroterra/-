b38d081677f8aca224d65d4b890cc2b8ea3733d7
Takphantom <miroterra@naver.com>
2022-02-21 18:33:07 +0900

    슬라이드완성

--- js/Slideshow_loop.js
+++ js/Slideshow_loop.js
@@ -1,13 +1,12 @@
 // 변수 지정
 const slides = document.querySelector('.ban .ban_slide'),
   slide = document.querySelectorAll('.ban .ban_slide li'),
-  currentIdx = 0, //슬라이드 파악
   slideCount = slide.length, //슬라이드 갯수 파악
   prevBtn = document.querySelector('.prev'),
   nextBtn = document.querySelector('.next');
-let slideWidth = 330,
-  slideMargin = 32;
-
+let slideWidth = 300,
+  slideMargin = 30,
+  currentIdx = 0; //슬라이드 파악
 makeClone(); //복사본 만들기
 // 복사본을 만들어서 앞or 뒤에 추가
 function makeClone() {
@@ -24,12 +23,64 @@ function makeClone() {
     // .prepend() 내용 앞에 요소를 추가하는 함수
     slides.prepend(cloneSlide);
   }
-  updateWidth(); // 복사한 모든 항목 ul의 총 길이값을 지정하는 함수
+  updateWidth(); // 슬라이드의 총 길이값 선언
+  setInitialPos(); //슬라이드의 초기 위치 설정 선언
+  setTimeout(function () {
+    //translate로 이동 하는게 보이기때문에
+    slides.classList.add('animated');
+  }, 100);
 }
 
 function updateWidth() {
+  // 복사한 모든 항목 ul의 총 길이값을 지정하는 함수
   let currentSlides = document.querySelectorAll('.ban .ban_slide li');
   let newSlideCount = currentSlides.length;
-  let newWidth = slideWidth * newSlideCount + 'px';
+  let newWidth = (slideWidth + slideMargin) * newSlideCount - slideMargin + 'px';
   slides.style.width = newWidth;
 }
+function setInitialPos() {
+  //슬라이드의 초기 위치 설정
+  let initialTranslateValue = -(slideWidth + slideMargin) * slideCount;
+  slides.style.transform = 'translateX(' + initialTranslateValue + 'px)';
+}
+nextBtn.addEventListener('click', function () {
+  moveslide(currentIdx + 1);
+});
+prevBtn.addEventListener('click', function () {
+  moveslide(currentIdx - 1);
+});
+function moveslide(num) {
+  slides.style.left = -num * (slideWidth + slideMargin) + 'px';
+  currentIdx = num;
+  if (currentIdx == slideCount || currentIdx == -slideCount) {
+    setTimeout(function () {
+      slides.classList.remove('animated');
+      slides.style.left = '0px';
+      currentIdx = 0;
+    }, 500);
+    setTimeout(function () {
+      slides.classList.add('animated');
+    }, 600);
+  }
+}
+
+//자동슬라이드 만들기
+let timer = undefined;
+function autoSlide() {
+  if (timer == undefined) {
+    timer = setInterval(function () {
+      moveslide(currentIdx + 1);
+    }, 3000);
+  }
+}
+autoSlide();
+function stopSlide() {
+  clearInterval(timer);
+  timer = undefined;
+}
+document.querySelector('#cont_ban').addEventListener('mouseenter', function () {
+  stopSlide();
+});
+document.querySelector('#cont_ban').addEventListener('mouseleave', function () {
+  autoSlide();
+});
