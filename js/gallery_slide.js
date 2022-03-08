b6d334a6184cd4c523c72216df3bb042dee5acdf
Takphantom <miroterra@naver.com>
2022-03-08 21:29:53 +0900

    갤러리 이동1

---
+++ js/gallery_slide.js
@@ -0,0 +1,17 @@
+// function slideOn() {
+//   const gallerySlides = document.querySelector('.gallery_slide');
+//   const slideItem = document.querySelectorAll('.mySlide');
+//   const firstEl = slideItem[0];
+//   firstEl.classList.add('showing');
+// }
+// slideOn();
+// const timeSlide = setInterval(autoSlide, 5000);
+// function autoSlide() {
+//   const currentItem = document.querySelector('.showing');
+//   currentItem.classList.remove('showing');
+//   if (!currentItem.nextElementSibling) {
+//     slideItem[0].classList.add('showing');
+//   } else {
+//     currentItem.nextElementSibling.classList.add('showing');
+//   }
+// }
