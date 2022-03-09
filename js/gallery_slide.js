eef8de0276090bee9ed8a9ba40c19a2a3e11e10b
Takphantom <miroterra@naver.com>
2022-03-09 21:41:21 +0900

    갤러리 스크립트

--- js/gallery_slide.js
+++ js/gallery_slide.js
@@ -1,17 +1,43 @@
-// function slideOn() {
-//   const gallerySlides = document.querySelector('.gallery_slide');
-//   const slideItem = document.querySelectorAll('.mySlide');
-//   const firstEl = slideItem[0];
-//   firstEl.classList.add('showing');
-// }
-// slideOn();
-// const timeSlide = setInterval(autoSlide, 5000);
-// function autoSlide() {
-//   const currentItem = document.querySelector('.showing');
-//   currentItem.classList.remove('showing');
-//   if (!currentItem.nextElementSibling) {
-//     slideItem[0].classList.add('showing');
-//   } else {
-//     currentItem.nextElementSibling.classList.add('showing');
-//   }
-// }
+function slideOn() {
+  const gallerySlides = document.querySelector('.gallery_slide');
+  const slideItem = document.querySelectorAll('.mySlide');
+  const firstEl = slideItem[0];
+  firstEl.classList.add('showing');
+  const timeSlide = setInterval(autoSlide, 4000);
+
+  function slideStart(isThat) {
+    if (isThat) {
+      let clickBtn = 'previous';
+    } else {
+      let clickBtn = 'next';
+    }
+    const currentItem = document.querySelector('.showing');
+    currentItem.classList.remove('showing');
+    if (clickBtn === 'next') {
+      if (!currentItem.nextElementSibling) {
+        slideItem[0].classList.add('showing');
+      } else {
+        currentItem.nextElementSibling.classList.add('showing');
+      }
+    } else if (clickBtn === 'previous') {
+      if (!currentItem.previousElementSibling) {
+        slideItem[slideItem.length - 1].classList.add('showing');
+      } else {
+        currentItem.previousElementSibling.classList.add('showing');
+      }
+    }
+  }
+  const nextBtn = document.querySelector('#btn_next');
+  const prevBtn = document.querySelector('#btn_prev');
+  nextBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    clearInterval(timeSlide);
+    slideStart();
+  });
+  prevBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    clearInterval(timeSlide);
+    slideStart('');
+  });
+}
+slideOn();
